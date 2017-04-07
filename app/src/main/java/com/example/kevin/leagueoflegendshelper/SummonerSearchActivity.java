586ad967998a4b27a3e87d1b9ff38b19305b06cb
Kevin Camacho <Kevin Camacho>
2017-04-06 23:51:32 -0400

    First commit, freshly created Android files

---
+++ app/src/main/java/com/example/kevin/leagueoflegendshelper/SummonerSearchActivity.java
@@ -0,0 +1,13 @@
+package com.example.kevin.leagueoflegendshelper;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SummonerSearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_summoner_search);
+    }
+}
