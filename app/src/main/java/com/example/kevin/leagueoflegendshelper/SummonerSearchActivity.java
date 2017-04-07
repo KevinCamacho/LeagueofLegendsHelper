18b0337a12f7f6d3bbb0751e749092ff56e8e540
Kevin <kpcamacho@hotmail.com>
2017-04-07 00:10:43 -0400

    Updated dependencies

--- app/src/main/java/com/example/kevin/leagueoflegendshelper/SummonerSearchActivity.java
+++ app/src/main/java/com/example/kevin/leagueoflegendshelper/SummonerSearchActivity.java
@@ -10,7 +10,5 @@ public class SummonerSearchActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_summoner_search);
 
-
-        //asvawvawerv
     }
 }
