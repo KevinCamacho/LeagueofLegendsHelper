4cf1c17695fd98e6ea41901f4a03d3eacb424f1a
Kevin Camacho <Kevin Camacho>
2017-04-06 23:58:33 -0400

    test

--- app/src/main/java/com/example/kevin/leagueoflegendshelper/SummonerSearchActivity.java
+++ app/src/main/java/com/example/kevin/leagueoflegendshelper/SummonerSearchActivity.java
@@ -9,5 +9,8 @@ public class SummonerSearchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_summoner_search);
+
+
+        //asvawvawerv
     }
 }
