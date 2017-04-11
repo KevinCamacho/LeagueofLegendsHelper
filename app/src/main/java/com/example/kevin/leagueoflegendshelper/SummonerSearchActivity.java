ca92ccb62066f53af87282dba534cf0c10d5f243
Kevin <kpcamacho@hotmail.com>
2017-04-11 16:24:34 -0400

    Added java and layout files for Champion and Item list activities. Edited themes and colors.

--- app/src/main/java/com/example/kevin/leagueoflegendshelper/SummonerSearchActivity.java
+++ app/src/main/java/com/example/kevin/leagueoflegendshelper/SummonerSearchActivity.java
@@ -2,13 +2,24 @@ package com.example.kevin.leagueoflegendshelper;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
 
 public class SummonerSearchActivity extends AppCompatActivity {
+
+    private Toolbar toolBar;
+    private TextView toolBarTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_summoner_search);
 
+        toolBar = (Toolbar) findViewById(R.id.toolBar);
+        setSupportActionBar(toolBar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+
+        toolBarTitle = (TextView) findViewById(R.id.toolBar_Title);
     }
 }
