d2c0af675978c179cf3543b10ada3e193b90a08c
Kevin <kpcamacho@hotmail.com>
2017-04-11 20:45:35 -0400

    Update

---
+++ app/src/main/java/com/example/kevin/leagueoflegendshelper/ChampionListActivity.java
@@ -0,0 +1,13 @@
+package com.example.kevin.leagueoflegendshelper;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ChampionListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_champion_list);
+    }
+}
---
+++ app/src/main/java/com/example/kevin/leagueoflegendshelper/RiotPortal.java
@@ -0,0 +1,13 @@
+package com.example.kevin.leagueoflegendshelper;
+
+/**
+ * Created by Kevin on 4/11/2017.
+ */
+
+public class RiotPortal {
+
+    private static String APIKey1 = "RGAPI-8f158ea6-05a6-4765-8a5a-cfada6c5345e";
+    private static String APIKey2 = "RGAPI-3a3ee533-86cd-4a16-b7ad-820384e4f594";
+
+    
+}
