a6fa30c08dc305d118e01ea286ada9f68d9669c4
Kevin <kpcamacho@hotmail.com>
2017-04-20 14:51:29 -0400

    Updates to champion detail fragments.

--- app/src/main/java/com/example/kevin/leagueoflegendshelper/ChampionLoreDetailFragment.java
+++ app/src/main/java/com/example/kevin/leagueoflegendshelper/ChampionLoreDetailFragment.java
@@ -7,17 +7,39 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.HashMap;
+
 /**
  * Created by Kevin on 4/18/2017.
  */
 
 public class ChampionLoreDetailFragment extends Fragment {
+
+    private HashMap loreHash;
+
     public ChampionLoreDetailFragment() {
         //fragment constructor should be empty
     }
 
+    public static ChampionLoreDetailFragment newInstance(HashMap hash) {
+        Bundle bundle = new Bundle();
+        bundle.putSerializable("hash", hash);
+
+        ChampionLoreDetailFragment frag = new ChampionLoreDetailFragment();
+        frag.setArguments(bundle);
+
+        return frag;
+    }
+
+    private void readBundle(Bundle bundle) {
+        if (bundle != null) {
+            loreHash = (HashMap) bundle.getSerializable("hash");
+        }
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        readBundle(getArguments());
         return super.onCreateView(inflater, container, savedInstanceState);
     }
 }
